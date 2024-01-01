5a3583e385fd5e94ed066acf28c5ec192c401996
Satoki Hata <satokihata@gmail.com>
2024-01-01 16:39:44 +0900

    add handler

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	helloHandler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+	http.HandleFunc("/", helloHandler)
+
+	log.Println("server start")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
