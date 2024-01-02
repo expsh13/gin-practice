b164497603ebded63b49295c5b11e70047a1e8c6
Satoki Hata <satokihata@gmail.com>
2024-01-02 12:39:06 +0900

    add 構造体

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/expsh13/go-todo/handlers"
 	"github.com/gorilla/mux"
 )
+
+type TodoList struct {
+	ID        int
+	Title     string
+	CreatedAt time.Time
+}
 
 func main() {
 	r := mux.NewRouter()
@@ -16,6 +24,14 @@ func main() {
 	r.HandleFunc("/api/todo", handlers.AddTodoHandler).Methods(http.MethodPost)
 	r.HandleFunc("/api/todo/{id:[0-9]+}", handlers.UpdateTodoHandler).Methods(http.MethodPut)
 	r.HandleFunc("/api/todo/{id:[0-9]+}", handlers.DeleteTodoHandler).Methods(http.MethodDelete)
+
+	todo := TodoList{
+		ID:        1,
+		Title:     "title",
+		CreatedAt: time.Now(),
+	}
+
+	fmt.Println(todo)
 
 	log.Println("server start")
 	log.Fatal(http.ListenAndServe(":8080", r))
