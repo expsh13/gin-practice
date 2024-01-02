2895c679061e05a564b4e83deb43206ed2b45166
Satoki Hata <satokihata@gmail.com>
2024-01-02 11:43:51 +0900

    パス、クエリパラメータ

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,30 +1,56 @@
 package handlers
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello, world!\n")
 }
 
-// GET /todo
+// GET /api/todo
 func GetTodoHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "add todo\n")
+	queryMap := r.URL.Query()
+
+	var page int
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		page, err = strconv.Atoi(p[0])
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		page = 1
+	}
+
+	resString := fmt.Sprintf("get List (page %d)\n", page)
+	io.WriteString(w, resString)
+
 }
 
-// POST /todo
+// POST /api/todo
 func AddTodoHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "add todo\n")
 }
 
-// PUT /todo
+// PUT /api/todo/{id}
 func UpdateTodoHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "update todo\n")
+	listID, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil {
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+		return
+	}
+	resString := fmt.Sprintf("update No.%d\n", listID)
+	io.WriteString(w, resString)
 }
 
-// DELETE /todo
+// DELETE /api/todo/{id}
 func DeleteTodoHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "delete todo\n")
 }
--- main.go
+++ main.go
@@ -12,10 +12,10 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/todo", handlers.GetTodoHandler).Methods(http.MethodGet)
-	r.HandleFunc("/todo", handlers.AddTodoHandler).Methods(http.MethodPost)
-	r.HandleFunc("/todo", handlers.UpdateTodoHandler).Methods(http.MethodPut)
-	r.HandleFunc("/todo", handlers.DeleteTodoHandler).Methods(http.MethodDelete)
+	r.HandleFunc("/api/todo", handlers.GetTodoHandler).Methods(http.MethodGet)
+	r.HandleFunc("/api/todo", handlers.AddTodoHandler).Methods(http.MethodPost)
+	r.HandleFunc("/api/todo/{id:[0-9]+}", handlers.UpdateTodoHandler).Methods(http.MethodPut)
+	r.HandleFunc("/api/todo/{id:[0-9]+}", handlers.DeleteTodoHandler).Methods(http.MethodDelete)
 
 	log.Println("server start")
 	log.Fatal(http.ListenAndServe(":8080", r))
