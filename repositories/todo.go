e48e44e1977f2d3e250f9ff9ad0ed8d86c6150c8
Satoki Hata <satokihata@gmail.com>
2024-01-10 08:40:47 +0900

    add repositories

---
+++ repositories/todo.go
@@ -0,0 +1,80 @@
+package repositories
+
+import (
+	"database/sql"
+
+	"github.com/expsh13/go-todo/models"
+)
+
+const (
+	todoNumPerPage = 5
+)
+
+// GET /api/todo
+func SelectTodoList(db *sql.DB, page int) ([]models.TodoList, error) {
+	const sqlStr = `
+		select todo_id, title
+		from todo
+		limit ? offset ?;`
+
+	rows, err := db.Query(sqlStr, todoNumPerPage, ((page - 1) * todoNumPerPage))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	todoArray := make([]models.TodoList, 0)
+	for rows.Next() {
+		var todo models.TodoList
+		rows.Scan(&todo.ID, &todo.Title)
+		todoArray = append(todoArray, todo)
+	}
+
+	return todoArray, nil
+
+}
+
+// POST /api/todo
+func InsertTodo(db *sql.DB, todo models.TodoList) (models.TodoList, error) {
+	const sqlStrInsert = `
+	insert into todo (title, created_at) values
+	(?, now());
+	`
+	result, err := db.Exec(sqlStrInsert, todo.Title)
+	if err != nil {
+		return models.TodoList{}, err
+	}
+
+	id, _ := result.LastInsertId()
+
+	var newTodo models.TodoList
+	newTodo.Title = todo.Title
+	newTodo.ID = int(id)
+
+	return newTodo, nil
+}
+
+// PUT /api/todo/{id}
+func UpdateTodo(db *sql.DB, todoId int, todo models.TodoList) error {
+	const sqlStr = `
+	update todo set title = ? where todo_id = ?;
+	`
+	_, err := db.Exec(sqlStr, todo.Title, todoId)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DELETE /api/todo/{id}
+func DeleteTodo(db *sql.DB, todoId int) error {
+	const sqlStr = `
+	delete from todo where todo_id = ?;
+	`
+	_, err := db.Exec(sqlStr, todoId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
