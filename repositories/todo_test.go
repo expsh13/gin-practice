39843ddcf44860e85b048205ce7e462cc280ec23
Satoki Hata <satokihata@gmail.com>
2024-01-15 23:12:08 +0900

    add test

--- repositories/todo_test.go
+++ repositories/todo_test.go
@@ -1,23 +1,61 @@
 package repositories_test
 
 import (
+	"database/sql"
+	"fmt"
 	"testing"
 
 	"github.com/expsh13/go-todo/models"
+	"github.com/expsh13/go-todo/repositories"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // SelectArticleDetail 関数のテスト
 func TestSelectArticleDetail(t *testing.T) {
-	expected := []models.TodoList{
-		// (定義略)
-
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		t.Fatal(err)
 	}
-	//		got, err := repositories.SelectArticleDetail([適切な引数])
-	//		if err != nil {
-	//			t.Fatal(err)
-	//	}
-	//
-	// if got != expected {
-	// t.Errorf("get %s but want %s\n", got, expected)
-	// }
+	defer db.Close()
+
+	tests := []struct {
+		testTitle string
+		expected  models.TodoList
+	}{
+		{
+			testTitle: "test1",
+			expected: models.TodoList{
+				ID:    7,
+				Title: "firstPost",
+			},
+		},
+		{
+			testTitle: "test2",
+			expected: models.TodoList{
+				ID:    8,
+				Title: "update tes",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.testTitle, func(t *testing.T) {
+			got, err := repositories.SelectTodoDetail(db, test.expected.ID)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if got.ID != test.expected.ID {
+				t.Errorf("ID: get %d but want %d\n", got.ID, test.expected.ID)
+			}
+			if got.Title != test.expected.Title {
+				t.Errorf("Title: get %s but want %s\n", got.Title, test.expected.Title)
+			}
+		})
+	}
+
 }
